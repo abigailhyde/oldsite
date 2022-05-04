8254db26995cabb0c2a6c543e3070577cddd20fd
AbigailGSmith <abbygsmith21@gmail.com>
2022-05-03 17:57:56 -1000

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'yellow': '#F6BD60',
+      'offwhite': '#F7EDE2',
+      'lightpink': '#F5CAC3',
+      'green': '#84A59D',
+      'darkpink': '#EF6D6A',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
